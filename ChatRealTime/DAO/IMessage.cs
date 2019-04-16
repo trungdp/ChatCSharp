8a0e1509aa55ad22931982336144a64deb038246
trungdp <trungdp.labianlabs@gmail.com>
2019-04-16 11:45:16 +0700

    dd

---
+++ ChatRealTime/DAO/IMessage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    interface IMessage
+    {
+        int ID
+        {
+            get;
+            set;
+        }
+        object Content
+        {
+            get;
+            set;
+        }
+        int SendAt
+        {
+            get;
+            set;
+        }
+        int IDSender
+        {
+            get;
+            set;
+        }
+    }
+}
