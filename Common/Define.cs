8ee30a189b709a16bdef8cf88131316a6ce528ef
trungdp <trungdp.labianlabs@gmail.com>
2019-05-08 15:24:59 +0700

    add icon

---
+++ Common/Define.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Define
+    {
+        public IPAddress iPAddress = IPAddress.Parse("127.0.0.1");
+        public int port = 5000;
+    }
+}
